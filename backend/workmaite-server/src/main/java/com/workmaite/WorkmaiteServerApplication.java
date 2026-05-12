a50a279ac2c89891aa3d6ba5cbf72fc6c67811a4
daye <myannnng@gmail.com>
2026-05-12 20:58:23 +0900

    feat: Spring Boot 초기 프로젝트 세팅

---
+++ backend/workmaite-server/src/main/java/com/workmaite/WorkmaiteServerApplication.java
@@ -0,0 +1,13 @@
+package com.workmaite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmaiteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmaiteServerApplication.class, args);
+	}
+
+}
---
+++ backend/workmaite-server/src/test/java/com/workmaite/WorkmaiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workmaite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmaiteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
