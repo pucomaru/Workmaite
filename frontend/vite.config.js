c25a010a3f6781761f499a504cda6d1ac208191b
pucomaru <myannnng@gmail.com>
2026-05-29 10:33:30 +0900

    fix: proxy local 버전 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,15 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
+      '/api/v1': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        ws: true,
+      },
       // LiveKit 신호 서버 프록시 (CORS 우회)
       '/livekit-signal': {
         target: 'http://localhost:7880',
